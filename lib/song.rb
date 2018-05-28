cded57d861f777fed2bb7fd3e71a54833047d55a
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-28 13:32:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,10 @@
-class Song
-  attr_accessor :name, :artist_name
-  @@all = []
 
+class Song
+  
+  attr_accessor :name, :artist_name
+  
+  @@all = []
+  
   def self.all
     @@all
   end
@@ -9,5 +12,20 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def initialize(name = nil, artist_name = nil)
+    @name = name
+    @artist_name = artist_name
+  end
+  
+  def self.create
+    @@all << self.new
+    @@all[-1]
+  end
+  
+  def self.new_by_name(name)
+    @name = name
+    @@all << self.new(@name)
+    self.all.detect{|song| song.name == name}
+  end
 end
